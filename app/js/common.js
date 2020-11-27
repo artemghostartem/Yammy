513df694df9750e852c9562e161c6647e33d4ccc
Davyd Tevzadze <pawtod20@gmail.com>
2020-11-27 19:59:04 +0200

    header debugging & sticky header functions & dropdown languages

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,48 @@
-$(function() {
+$(document).ready(function() {
 
+	$(window).on('scroll', function () {
+		headerSticky()
+	})
 
+	// header functions
 
-});
+	function headerSticky() {
+
+		let header = $('header')
+		let mainScreen = $('.opening-section')
+		let headerOffsetTop = header.offset().top
+		let mainScreenHeight = mainScreen.height()
+		
+		if ( headerOffsetTop > 100 && (document.querySelector('header').classList.contains('active') == false )) {
+			header.addClass('active')
+			console.log('added')
+		} else if ( headerOffsetTop < 100 && document.querySelector('header').classList.contains('active')) {
+			header.removeClass('active')
+			console.log('removed')
+		}
+
+		if (headerOffsetTop > mainScreenHeight && (document.querySelector('.main_button').classList.contains('active') == false )) {
+			$('.main_button').addClass('active')
+		} else if (headerOffsetTop < mainScreenHeight && document.querySelector('.main_button').classList.contains('active')) {
+			$('.main_button').removeClass('active')
+
+		}
+	}
+	//language dropdown
+
+	dropdownLanguage();
+
+	function dropdownLanguage() {
+		let droppdownLanguages = $('.language-menu')
+		let dropdown = $('.language')
+
+		droppdownLanguages.css("display", "flex")
+		droppdownLanguages.hide();
+
+		dropdown.on('click', function() {
+			droppdownLanguages.fadeToggle(0)
+			dropdown.toggleClass('active')
+		})
+	}
+	
+})
